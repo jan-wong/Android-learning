37d2609aa8ccb2a2d274c07ab39c7522338a150c
复读机 <543050768@qq.com>
2019-01-13 17:53:34 +0800

    intent 穿梭

--- ActivityTest/app/src/main/java/com/example/wangjiang/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/wangjiang/activitytest/FirstActivity.java
@@ -1,7 +1,11 @@
 package com.example.wangjiang.activitytest;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,11 +22,43 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You clicked Button 1",
-                        Toast.LENGTH_SHORT).show();
+                // 弹窗文字
+//                Toast.makeText(FirstActivity.this, "You clicked Button 1",
+//                        Toast.LENGTH_SHORT).show();
+                // 终止程序
 //                finish();
+                // 启动活动secondactivity
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                Intent intent = new Intent("com.example.wangjiang.activitytest.ACTION_START");
+//                intent.addCategory("com.example.wangjiang.activitytest.MY_CATEGORY");
+//                startActivity(intent);
+                // 打开浏览器特定网页
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent);
+                // 传递数据
+//                String data = "hello SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                intent.putExtra("extra_data", data);
+//                startActivity(intent);
+                // 返回数据
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 
     @Override
@@ -44,4 +80,5 @@ public class FirstActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }
